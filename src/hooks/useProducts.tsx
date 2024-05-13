2314138b31d21061dc2d7884792405a4ab173d43
Luciana <luciana.holzmann@gmail.com>
2024-05-14 00:09:15 +0200

    [FEAT] Pagination logic

--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -1,28 +1,29 @@
-import  { useState } from 'react'
+import { useState } from 'react';
 import { DataAPI } from '../types/productsAPI.type';
 
 const useProducts = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [products, setProducts] = useState< [] | DataAPI[]>([]);
-    const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState<[] | DataAPI[]>([]);
+  const [error, setError] = useState('');
 
-    const getProducts = async() => {
-        setIsLoading(true);
+  const getProducts = async () => {
+    setIsLoading(true);
 
-        try{
-            const response = await fetch('https://api.escuelajs.co/api/v1/products');
-            const data = await response.json();
-            setProducts(data)
-        } catch(error){
-            setError(error)
-        }finally{
-            setIsLoading(false);
-        }
+    try {
+      const response = await fetch('https://api.escuelajs.co/api/v1/products');
+      const data = await response.json();
+
+      // FILTRAR SOLO 9 PRODUCTOS DE TODO LO QUE RECIBO
+      const filteredProducts = data.slice(0, 9);
+      setProducts(filteredProducts);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-  return {isLoading, products,error, getProducts
+  return { isLoading, products, error, getProducts };
+};
 
-  }
-}
-
-export default useProducts
+export default useProducts;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,75 @@
-import { useEffect } from 'react';
-import useProducts from '../hooks/useProducts'
+import { useState, useEffect } from 'react';
+import useProducts from '../hooks/useProducts';
 import Card from '../components/Card.tsx';
-import Pagination from '../components/Pagination.tsx'
-
+import NavbarHome from '../components/NavbarHome.tsx';
 
 const Home = () => {
- const {isLoading, products, error, getProducts} = useProducts();
+  const { isLoading, products, error, getProducts } = useProducts();
+
+  const [paginaActual, setPaginaActual] = useState(1);
+  const [productosPorPagina, setProductosPorPagina] = useState(3);
+  const [productosActuales, setProductosActuales] = useState([]);
 
   useEffect(() => {
-     getProducts()
+    getProducts();
   }, []);
 
+  useEffect(() => {
+    const startIndex = (paginaActual - 1) * productosPorPagina;
+    const endIndex = startIndex + productosPorPagina;
+    setProductosActuales(products.slice(startIndex, endIndex));
+  }, [paginaActual, products]);
+
+  const cambiarPagina = (nuevaPagina) => {
+    setPaginaActual(nuevaPagina);
+  };
 
   return (
-    <div className='containerHome'>
-      {
-        products.length > 0 && products.map((product, index) => (
-          <Card key={index} product={product} isLoading={isLoading}/>
-        ))
-      }
+    <div>
+      <div>
+        <NavbarHome />
+      </div>
 
-      <Pagination/>
-      
+      <div className="containerCards">
+        {isLoading ? (
+          <p>Cargando productos...</p>
+        ) : error ? (
+          <p>Error: {error.message}</p>
+        ) : productosActuales.length > 0 && (
+          productosActuales.map((producto, index) => (
+            <Card key={index} product={producto} isLoading={isLoading} />
+          ))
+        )}
+      </div>
+
+      <div className="pagination">
+        <button
+          type="button"
+          id="1"
+          disabled={paginaActual === 1}
+          onClick={() => cambiarPagina(1)}
+        >
+          1
+        </button>
+        <button
+          type="button"
+          id="2"
+          onClick={() => cambiarPagina(paginaActual + 1)}
+        >
+          2
+        </button>
+        <button
+          type="button"
+          id="3"
+          onClick={() => cambiarPagina(paginaActual + 2)}
+        >
+          3
+        </button>
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
